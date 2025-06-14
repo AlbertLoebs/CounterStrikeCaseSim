4796959fdbb465ba7b59121f74c59db388115c87
Albert Loebs <albertloebs1@gmail.com>
2025-06-14 13:51:06 -0700

    more organization and some new arrays

--- index.js
+++ index.js
@@ -2,13 +2,49 @@ const maxFloat = 0.99999999999999;
 const minFloat = 0.00000000000001;
 const maxSeed = 1000;
 const minSeed = 0;
+const rarityOdds = {"Mil-Spec Grade"   : 0.7992,
+                    "Restricted"       : 0.1598,
+                    "Classified"       : 0.0320,
+                    "Exceedingly Rare" : 0.0026
+                    };
+const statTrakOdds = 0.1;
+
+const allKnifes = [
+    "Karambit",
+    "Bayonet",
+    "Gut Knife",
+    "Flip Knife",
+    "M9 Bayonet",
+    "Bowie Knife",
+    "Shadow Daggers",
+    "Falchion Knife",
+    "Butterfly Knife",
+    "Huntsman Knife",
+    "Flip Knife",
+    "Talon Knife",
+    "Stiletto Knife",
+    "Navaja Knife",
+    "Ursus Knife",
+    "Survival Knife",
+    "Skeleton Knife",
+    "Paracord Knife",
+    "Nomad Knife",
+    "Kukri Knife"
+]
+
+const knifePool = {
+    "CS:GO Weapon Case" : {
+        "Bayonet" : ["Fade", "Slaughter", "Case Hardened", "★", "Crimson Web", "Blue Steel", "Night", "Stained", 
+                     "Boreal Forest", "Urban Masked", "Scorched", "Forest DDPAT", "Safari Mesh"]
+    }
+}
 
 class Weapon {
     constructor(name, rarity, float, condition, seed, statTrak){
         this.name = name;
         this.rarity = rarity;
         this.float = float;
-        this.condition = condition;
+        this.condition = setCondition();
         this.seed = seed;
         this.statTrak = statTrak;
     }
@@ -24,17 +60,11 @@ class Weapon {
     }
 
     setCondition(){
-        if (this.float > 0 && this.float <= 0.07){
-            this.condition = "Factory New";
-        } else if(this.float > 0.07 && this.float <= 0.15){
-            this.condition = "Minimal Wear";
-        } else if(this.float > 0.15 && this.float <= 0.38){
-            this.condition = "Field-Tested";
-        } else if(this.float > 0.38 && this.float <= 0.45){
-            this.condition = "Well-Worn"
-        } else {
-            this.condition = "Battle-Scarred";
-        }
+        if (this.float <= 0.07) return "Factory New";
+        if (this.float <= 0.15) return "Minimal Wear";
+        if (this.float <= 0.38) return "Field-Tested";
+        if (this.float <= 0.45) return "Well-Worn";
+        return "Battle-Scarred";
     }
 
 }
@@ -43,25 +73,33 @@ class Case{
     constructor(caseName, caseWeapons, rarityOdds, statTrakOdds){
         this.caseName = caseName;
         this.caseWeapons = caseWeapons;
-        this.rarityOdds = rarityOdds;
-        this.statTrakOdds = statTrakOdds;
-    }
-}
-
-class WeaponCase extends Case{
-    constructor(caseName = "CS:GO Weapon Case",
-                caseWeapons = ["AWP | Lightning Strike", "AK-47 | Case Hardened", "Desert Eagle | Hypotic",
-                             "Glock-18 | Dragon Tatto", "USP-S | Dark Water", "M4A1-S | Dark Water",
-                             "SG 553 | Ultraviolet", "MP7 | Skulls", "AUG | Wings"], 
-                rarityOdds = {"Mil-Spec Grade"   : 0.7992,
+        this.rarityOdds = {   "Mil-Spec Grade"   : 0.7992,
                               "Restricted"       : 0.1598,
                               "Classified"       : 0.0320,
                               "Exceedingly Rare" : 0.0026
-                            },
-                statTrakOdds = 0.1,
-    
-    ){
-        super(caseName, caseWeapons, rarityOdds, statTrakOdds);
+                            };
+        this.statTrakOdds = 0.1;
     }
+}
 
+class WeaponCase extends Case {
+    constructor() {
+        super("CS:GO Weapon Case", {
+            "Mil-Spec Grade": [
+                "SG 553 | Ultraviolet", "MP7 | Skulls", "AUG | Wings"
+            ],
+            "Restricted": [
+                "Glock-18 | Dragon Tattoo", "USP-S | Dark Water", "M4A1-S | Dark Water"
+            ],
+            "Classified": [
+                "Desert Eagle | Hypnotic", "AK-47 | Case Hardened"
+            ],
+            "Covert": [
+                "AWP | Lightning Strike"
+            ], 
+            "Exceedingly Rare": [
+
+            ]
+        });
+    }
 }
